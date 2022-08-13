cca8ab0d6c2154bafbbebc1d1262247b397ef4b2
MRMDprjkt° <87246800+Merma1dmanPrjct@users.noreply.github.com>
2022-08-13 10:36:07 +0700

    Update aosp_lavender.mk

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -8,15 +8,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Proton stuff
-$(call inherit-product, vendor/proton/build/common.mk)
+# Inherit some stuff
+$(call inherit-product, vendor/derp/build/common_full_phone.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_lavender
+PRODUCT_NAME := derp_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
@@ -26,3 +26,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lavender" \
     PRODUCT_NAME="lavender"
+
+USE_LEGACY_BOOTANIMATION := true
