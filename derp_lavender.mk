38ead5396b7935723ce61f2f3f98763b25beb0a0
MRMDprjkt° <87246800+Merma1dmanPrjct@users.noreply.github.com>
2022-08-15 15:32:08 +0700

    Update derp_lavender.mk

--- derp_lavender.mk
+++ derp_lavender.mk
@@ -8,8 +8,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some stuff
+# Inherit some common PixelExperience stuff
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
